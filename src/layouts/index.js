f2a4bbde114f38ec306401092f08e3bbf76ba956
Dan Laush <dan.laush@gmail.com>
2018-07-27 23:21:34 +1000

    Initial commit home page wow graphql wow

---
+++ src/layouts/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import base from './base.css'
+import Container from '../components/container'
+
+class Template extends React.Component {
+  render() {
+    const { location, children } = this.props
+    let header
+
+    let rootPath = `/`
+    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
+      rootPath = __PATH_PREFIX__ + `/`
+    }
+
+    return (
+      <Container>
+        {children()}
+      </Container>
+    )
+  }
+}
+
+export default Template
---
+++ src/pages/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+
+class RootIndex extends React.Component {
+  render() {
+    // const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const person = get(this, 'props.data.contentfulPerson')
+
+    return (
+      <div>{/*I hate this div so much. This is why we can't have nice documents*/}
+        <Helmet title={person.name} />
+        {/* <Hero data={author.node} /> */}
+        <div className="wrapper">
+          <h1>{person.name}</h1>
+          <ul>
+              <li>
+                <Link to={`/about`}>
+                  About <span className="sr-only">{person.name}</span>
+                </Link>
+              </li>
+              <li>
+                  <a 
+                      href={`https://twitter.com/${person.twitter}`}>
+                      <span className="sr-only">Twitter link: </span>
+                      {person.twitter}
+                  </a>
+              </li>
+              <li>
+                  <a 
+                      href={`mailto:${person.email}`}>
+                      <span className="sr-only">Email link: </span>
+                      {person.email}
+                  </a>
+              </li>
+          </ul>
+          <h2>Things</h2>
+          <ul>
+            <li>
+              <Link to={`/projects`}>
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link to={`/resume`}>
+                Resume
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RootIndex
+
+export const pageQuery = graphql`
+  query HomeQuery {
+    contentfulPerson {
+        name
+        twitter
+        email
+    }
+  }
+`
