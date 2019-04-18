747ac77a53f6819e98867dc20f5678a72d5b8385
Dan Laush <dan.laush@gmail.com>
2019-04-18 22:02:16 +1000

    Fix h1 margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,43 +15,40 @@ class RootIndex extends React.Component {
         <Helmet title={person.name} htmlAttributes={
           {"lang": "en"}
         } />
-        {/* <Hero data={author.node} /> */}
-        <div className="wrapper">
-          <h1>{person.name}</h1>
-          <ul>
-            <li>{person.title}</li>
-            <li>
-              <Link to={`/about`}>
-                About <span className="sr-only">{person.name}</span>
-              </Link>
-            </li>
-            <li>
-              <a href={`https://twitter.com/${person.twitter}`}>
-                <span className="sr-only">Twitter link: </span>
-                {person.twitter}
-              </a>
-            </li>
-            <li>
-              <a href={`mailto:${person.email}`}>
-                <span className="sr-only">Email link: </span>
-                {person.email}
-              </a>
-            </li>
-          </ul>
-          <h2>Things</h2>
-          <ul>
-            <li>
-              <Link to={`/projects`}>
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link to={`/resume`}>
-                Resume
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <h1>{person.name}</h1>
+        <ul>
+          <li>{person.title}</li>
+          <li>
+            <Link to={`/about`}>
+              About <span className="sr-only">{person.name}</span>
+            </Link>
+          </li>
+          <li>
+            <a href={`https://twitter.com/${person.twitter}`}>
+              <span className="sr-only">Twitter link: </span>
+              {person.twitter}
+            </a>
+          </li>
+          <li>
+            <a href={`mailto:${person.email}`}>
+              <span className="sr-only">Email link: </span>
+              {person.email}
+            </a>
+          </li>
+        </ul>
+        <h2>Things</h2>
+        <ul>
+          <li>
+            <Link to={`/projects`}>
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to={`/resume`}>
+              Resume
+            </Link>
+          </li>
+        </ul>
       </Container>
     )
   }
